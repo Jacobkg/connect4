b15ccd5ab858967a48fc019646fdacede347fc97
Jacob Green <jacob@nationbuilder.com>
2013-11-29 13:36:55 -0800

    Refactor. Fix bug in heuristic function.

--- computer.rb
+++ computer.rb
@@ -1,10 +1,15 @@
 class Computer
 
+  attr_reader :player
+  def initialize(player)
+    @player = player
+  end
+
   def pick_move(board)
     best_move = nil
     best_score = -1_000_000
     board.available_moves.each do |possible_move|
-      test_board = board.drop_piece possible_move, "O"
+      test_board = board.drop_piece possible_move, player
       score = minimax(test_board, 5, -1_000, 1_000, false)
       puts "Move #{possible_move + 1} has score #{score}"
       if score > best_score
@@ -15,25 +20,32 @@ class Computer
     best_move
   end
 
-  def heuristic_score(board, player = "O")
-    count = 0
-    0.upto(7) do |x|
-      0.upto(7) do |y|
-        next unless board.board_state[x][y] == other_player(player)
-        count += board.count_left(x, y, player)
-        count += board.count_right(x, y, player)
-        count += board.count_up(x, y, player)
-        count += board.count_down(x, y, player)
-        count += board.count_up_left(x, y, player)
-        count += board.count_down_right(x, y, player)
-        count += board.count_up_right(x, y, player)
-        count += board.count_down_left(x, y, player)
-      end
-    end
-    count
+  def score(board)
+    winner = board.winner
+    return heuristic_score(board) if winner.nil?
+    return 999 if winner == player
+    return -999
   end
 
   private
+
+    def heuristic_score(board)
+      count = 0
+      0.upto(7) do |x|
+        0.upto(7) do |y|
+          next if board.board_state[x][y] == other_player
+          count += board.count_left(x, y, player)
+          count += board.count_right(x, y, player)
+          count += board.count_up(x, y, player)
+          count += board.count_down(x, y, player)
+          count += board.count_up_left(x, y, player)
+          count += board.count_down_right(x, y, player)
+          count += board.count_up_right(x, y, player)
+          count += board.count_down_left(x, y, player)
+        end
+      end
+      count
+    end
 
     def minimax(board, depth, alpha, beta, maximizing_player)
       if depth == 0 || board.winner
@@ -41,14 +53,14 @@ class Computer
       end
       if maximizing_player
         board.available_moves.each do |possible_move|
-          test_board = board.drop_piece possible_move, "O"
+          test_board = board.drop_piece possible_move, player
           alpha = [alpha, minimax(test_board, depth - 1, alpha, beta, false)].max
           break if beta <= alpha
         end
         return alpha
       else
         board.available_moves.each do |possible_move|
-          test_board = board.drop_piece possible_move, "X"
+          test_board = board.drop_piece possible_move, other_player
           beta = [beta, minimax(test_board, depth - 1, alpha, beta, true)].min
           break if beta <= alpha
         end
@@ -56,15 +68,8 @@ class Computer
       end
     end
 
-    def score(board)
-      winner = board.winner
-      return heuristic_score(board) if winner.nil?
-      return 999 if winner == "O"
-      return -999
-    end
-
-    def other_player(player)
-      player == "X" ? "O" : "X"
+    def other_player
+      @player == "X" ? "O" : "X"
     end
 
 end
--- main.rb
+++ main.rb
@@ -10,13 +10,13 @@ end
 board = Board.new
 #board = board.drop_piece(4, "O")
 loop do
-  puts "Heuristic Score for O: #{Computer.new.heuristic_score(board, "O")}"
-  puts "Heuristic Score for X: #{Computer.new.heuristic_score(board, "X")}"
+  puts "Heuristic Score for O: #{Computer.new("O").score(board)}"
+  puts "Heuristic Score for X: #{Computer.new("X").score(board)}"
   board.print
   col = request_user_move
   board = board.drop_piece col, "X"
   break if board.winner
-  board = board.drop_piece Computer.new.pick_move(board), "O"
+  board = board.drop_piece Computer.new("O").pick_move(board), "O"
   break if board.winner
 end
 
